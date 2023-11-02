a7724aa9da5f734877a2a4acb874427785343e20
davidefossacecchi <davide.fossacecchi@gmail.com>
2023-11-02 22:24:11 +0100

    feat: adds a decorator to wrap both source descriptor and sourcelist descriptor

--- app/Services/Fetchers/SourceDescriptors/SourceDescriptorDecorator.php
+++ app/Services/Fetchers/SourceDescriptors/SourceDescriptorDecorator.php
@@ -2,7 +2,28 @@
 
 namespace App\Services\Fetchers\SourceDescriptors;
 
-class SourceDescriptorDecorator
-{
+use App\Services\Fetchers\Contracts\SourceDescriptorInterface;
+use App\Services\Fetchers\Contracts\SourceListDescriptorInterface;
+use App\Services\Ranges\DateRange;
 
+class SourceDescriptorDecorator implements SourceDescriptorInterface, SourceListDescriptorInterface
+{
+    public function __construct(private readonly SourceDescriptorInterface $source, private readonly SourceListDescriptorInterface $listDescriptor)
+    {
+    }
+
+    public function getResourceUrl(): string
+    {
+        return $this->source->getResourceUrl();
+    }
+
+    public function getRecordClass(): string
+    {
+        return $this->source->getRecordClass();
+    }
+
+    public function isValidSource(array|string $source, DateRange $range): bool
+    {
+        return $this->listDescriptor->isValidSource($source, $range);
+    }
 }
