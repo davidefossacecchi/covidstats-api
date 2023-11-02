31973fc60e34cc68001e9007066524806846d1c7
davidefossacecchi <davide.fossacecchi@gmail.com>
2023-11-02 22:25:08 +0100

    feat: adds a source list descriptor form pcm dpc repository

--- app/Services/Fetchers/SourceDescriptors/PcmDpcListDescriptor.php
+++ app/Services/Fetchers/SourceDescriptors/PcmDpcListDescriptor.php
@@ -2,7 +2,31 @@
 
 namespace App\Services\Fetchers\SourceDescriptors;
 
-class PcmDpcListDescriptor
+use App\Services\Fetchers\Contracts\SourceListDescriptorInterface;
+use App\Services\Ranges\DateRange;
+
+class PcmDpcListDescriptor implements SourceListDescriptorInterface
 {
+    private readonly string $regexp;
+    public function __construct(string $filePrefix)
+    {
+        $this->regexp = '/'.$filePrefix.'([0-9]{8})\.csv/';
+    }
+
+    public function isValidSource(array|string $source, DateRange $range): bool
+    {
+        if (0 === preg_match($this->regexp, $source, $matches)) {
+            return false;
+        }
+
+        $fileDatetime = \DateTime::createFromFormat('Ymd H:i:s', $matches[1].' 00:00:00');
+
+        if (empty($fileDatetime)) {
+            return false;
+        }
+
+        return $range->includes($fileDatetime);
+    }
+
 
 }
--- tests/SourceDescriptorsTest.php
+++ tests/SourceDescriptorsTest.php
@@ -1,6 +1,44 @@
 <?php
+namespace Tests;
+use App\Services\Fetchers\SourceDescriptors\PcmDpcListDescriptor;
+use App\Services\Ranges\DateRange;
 
-class SourceDescriptorsTest
+class SourceDescriptorsTest extends TestCase
 {
+    private PcmDpcListDescriptor $listDescriptor;
+    private DateRange $range;
+    protected function setUp(): void
+    {
+        $this->listDescriptor = new PcmDpcListDescriptor('dpc-covid19-ita-regioni-');
+        $this->range = new DateRange(\DateTime::createFromFormat('Y-m-d H:i:s', '2019-01-01 00:00:00'), new \DateTime());
+    }
 
+    public function testInvalidFilenameIsNotAccepted()
+    {
+        $invalidNames = ['just-an-invalid-file', 'dpc-covid19-ita-regioni-22224.csv'];
+        foreach ($invalidNames as $invalidName) {
+            $isValid = $this->listDescriptor->isValidSource($invalidName, $this->range);
+            $this->assertFalse($isValid);
+        }
+    }
+
+    public function testOutOfRangeDatesAreInvalid()
+    {
+        $invalidNames = ['dpc-covid19-ita-regioni-20180101.csv', 'dpc-covid19-ita-regioni-21000101.csv'];
+        foreach ($invalidNames as $invalidName) {
+            $isValid = $this->listDescriptor->isValidSource($invalidName, $this->range);
+            $this->assertFalse($isValid);
+        }
+    }
+
+    public function testValidNamesInRangeAreValid()
+    {
+        $lastDate = (new \DateTime())->sub(new \DateInterval('P1D'))->format('Ymd');
+
+        $invalidNames = ['dpc-covid19-ita-regioni-20190101.csv', 'dpc-covid19-ita-regioni-'.$lastDate.'.csv'];
+        foreach ($invalidNames as $invalidName) {
+            $isValid = $this->listDescriptor->isValidSource($invalidName, $this->range);
+            $this->assertTrue($isValid);
+        }
+    }
 }
